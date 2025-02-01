b0f77a25a822c7fdc99b00231f29e75ce98cfcbd
jonathan <jattar02@gmail.com>
2025-02-01 17:21:11 +0100

    vite base url

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+const isProd = process.env.NODE_ENV === "production";
+export default defineConfig({
+    base: isProd ? `threejs-svg-parser/dist/` : "",
+});
