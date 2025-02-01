636058a786f7f403f342330c9dc72c042384568c
jonathan <jattar02@gmail.com>
2025-02-01 18:04:17 +0100

    rename base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 const isProd = process.env.NODE_ENV === "production";
 export default defineConfig({
-    base: isProd ? `/threejs-svg-parser/dist/` : "/",
+    base: isProd ? `/threejs-svg-parser/` : "/",
 });
